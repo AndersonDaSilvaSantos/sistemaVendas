7913a0acae253dd66acee791139192b5d5324da0
Aluno <Aluno@172.30.1.48>
2019-09-17 20:16:47 -0300

    Teste

---
+++ src/test/java/com/dev/sistemaVendas/SistemaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sistemaVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
